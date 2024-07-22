334acc63ab707ac8f99a7a79dedd935da4df369e
Julio Lab <juliolab90@hotmail.com>
2024-07-22 15:17:00 -0600

    Improves functionality and adds styling

--- scripts/script.js
+++ scripts/script.js
@@ -32,21 +32,48 @@ function printDefs(data) {
 
    defs.innerText = "";
 
+   // Creates an <h3> to print the searched word
+   let searchedWord = document.createElement("h3");
+   defs.appendChild(searchedWord);
+   searchedWord.style.color = "goldenrod";
+   searchedWord.style.textTransform = "capitalize";
+   searchedWord.innerText = data[0].word;
+   
    // Creates a <p> for each definition
    for (let m = 0; m < data[0].meanings.length; m++) {
       for (let d = 0; d < data[0].meanings[m].definitions.length; d++) {
-         let par = document.createElement("p");
+         if (m === 0 && d === 0 && data[0].origin !== undefined) {
+            let originP = document.createElement("p");
+            defs.appendChild(originP);
+            // originP.style.fontSize = "larger";
+            originP.innerText = "Origin: " + data[0].origin;
+         }
+
+         if (d === 0) {
+            let syn_ant_p = document.createElement("p");
+            defs.appendChild(syn_ant_p);
+            syn_ant_p.style.margin = "1.3em 0 0.5em";
+            syn_ant_p.style.fontSize = "smaller";
+            syn_ant_p.innerText += "Synonyms: " + data[0].meanings[m].synonyms.join(', ') + "\nAntonyms: " + data[0].meanings[m].antonyms.join(', ');
+         }
+
+         let par = document.createElement("li");
 
          defs.appendChild(par);
-         
-         if (m === 0 && d === 0) { par.innerText = "Origin: " + data[0].origin + "\n\n"; }
 
-         par.innerText += "* [" + data[0].meanings[m].partOfSpeech + "] " + data[0].meanings[m].definitions[d].definition
-         + "\n- Example: " + data[0].meanings[m].definitions[d].example;
+         par.innerHTML += "<small>[" + data[0].meanings[m].partOfSpeech + "]</small> " + data[0].meanings[m].definitions[d].definition + "<br />";
 
-         if (d === data[0].meanings[m].definitions.length - 1) {
-            par.innerText += "\n\nSynonyms: " + data[0].meanings[m].synonyms.join(', ') + "\nAntonyms: " + data[0].meanings[m].antonyms.join(', ');
-         }
+         if (data[0].meanings[m].definitions[d].example !== undefined) {
+            par.innerHTML += "<small>e.g., <i>\"" + data[0].meanings[m].definitions[d].example + "\"</i></small><br /><br />";
+         } else if (d !== data[0].meanings[m].definitions.length - 1) { par.innerHTML += "<br />"; }
+
+         /* if (d === data[0].meanings[m].definitions.length - 1) {
+            let syn_ant_p = document.createElement("p");
+            defs.appendChild(syn_ant_p);
+            // syn_ant_p.style.margin = "0.7em 1em";
+            // syn_ant_p.style.fontSize = "smaller";
+            syn_ant_p.innerText += "Synonyms: " + data[0].meanings[m].synonyms.join(', ') + "\nAntonyms: " + data[0].meanings[m].antonyms.join(', ');
+         } */
       }
    }
 }
